5c1262f96021c243344403d57445e298e2510d04
Patrick Murphy <murph.pm@gmail.com>
2012-05-14 11:15:58 -0500

    Added quick sort and merge sort

--- Assignment 1/src/Sorting.java
+++ Assignment 1/src/Sorting.java
@@ -9,7 +9,7 @@ import javax.swing.JFileChooser;
 public class Sorting {
 
 	private static void processInput(){
-		File toParse = getFileViaChooser();
+		File toParse = null;
 		FileReader fr = null;
 		BufferedReader br = null;
 		String input = null;
@@ -19,6 +19,7 @@ public class Sorting {
 		
 		
 		try{
+			toParse = getFileViaChooser();
 			fr = new FileReader(toParse);
 			br = new BufferedReader(fr);
 			
@@ -70,8 +71,13 @@ public class Sorting {
 		System.arraycopy(inputArray, 0, mergeSortArray, 0, inputArray.length);
 		System.arraycopy(inputArray, 0, insertionSortArray, 0, inputArray.length);
 		System.arraycopy(inputArray, 0, radixSortArray, 0, inputArray.length);
-		
+		//TODO: Finish up sorting methods
 		insertionSortArray = insertionSort(insertionSortArray);
+		quickSortArray = quickSort(quickSortArray, 0, quickSortArray.length);
+		mergeSortArray = mergeSort(mergeSortArray, 0, mergeSortArray.length, new String[mergeSortArray.length]);
+		for(int i = 0; i < mergeSortArray.length; i++)
+			System.out.println(mergeSortArray[i]);
+		
 	}
 	
 	private static String[] insertionSort(String[] toSort){
@@ -86,6 +92,86 @@ public class Sorting {
 				j--;
 			}
 			toSort[j] = toInsert;
+		}
+		
+		return toSort;
+	}
+	
+	private static String[] mergeSort(String[] toSort, int minV, int maxV, String[] temp){
+		String[] toReturn = null;
+		int midV = 0;
+		if(maxV > minV+1){
+			midV = (maxV - minV) / 2 + minV;
+			
+			mergeSort(toSort, minV, midV, temp);
+			mergeSort(toSort, midV, maxV, temp);
+			toReturn = merge(toSort, minV, midV, maxV, temp);
+		}
+		return toReturn;
+	}
+	
+	private static String[] merge(String[] toSort, int minV, int midV, int maxV, String[] temp)
+	{
+		int currR = minV;
+		int currL = midV;
+		int currT = minV;
+		
+		while(currL < midV && currR < maxV){
+			if( toSort[currL].compareTo(toSort[currR]) <= 0){
+				temp[currT] = toSort[currL];
+				currL++;
+			}else{
+				temp[currT] = toSort[currR];
+				currR++;
+			}
+			currT++;
+		}
+		if(currL < midV){
+			while(currL < midV ){
+				temp[currT] = toSort[currL];
+				currT++;
+				currL++;
+			}
+		}else if(currR < maxV){
+			while(currR < maxV){
+				temp[currT] = toSort[currR];
+				currT++;
+				currR++;
+			}
+		}
+		for(int i = midV; i < maxV; i++)
+			toSort[i] = temp[i];
+		return toSort;
+	}
+	
+	private static String[] quickSort(String[] toSort, int minV, int maxV){
+		int newPivot = randomNum(minV, maxV);
+		String tmp = null;
+		int big = minV + 1;
+		int small = minV + 1;
+		
+		if(maxV > minV + 1){
+			tmp = toSort[minV];
+			toSort[minV] = toSort[newPivot];
+			toSort[newPivot] = tmp;
+			newPivot = minV;
+			
+			for(int i = minV + 1; i < maxV; i++){
+				if( toSort[i].compareTo(toSort[newPivot]) < 0 ){
+					tmp = toSort[big];
+					toSort[big] = toSort[i];
+					toSort[i] = tmp;
+					big++;
+				}
+			}
+			tmp = toSort[big-1];
+			toSort[big - 1] = toSort[newPivot];
+			toSort[newPivot] = tmp;
+			
+			newPivot = big - 1;
+			
+			quickSort(toSort, minV, newPivot);
+			quickSort(toSort, newPivot + 1, maxV);
 		}
 		
 		return toSort;
@@ -114,6 +200,10 @@ public class Sorting {
 		}
 		
 		return null;
+	}
+	
+	private static int randomNum(int min, int max){
+		return (int)(Math.random() * (max - min)) + min;
 	}
 	
 	public static void main(String[] args){
